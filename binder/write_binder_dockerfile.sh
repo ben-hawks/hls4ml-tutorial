cc4b6965bbb93332bbcb04704c8d3484c6a89621
“Ben <bhawks@fnal.gov>
2023-06-09 19:16:09 -0500

    update binder shim dockerfile script

--- binder/write_binder_dockerfile.sh
+++ binder/write_binder_dockerfile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #To make this repo work and uses the existing Docker image within a BinderHub instance, we need to create a shim Dockerfile that Binder/repo2docker picks up on. 
 #This is intended to be ran via a github action
-cd ./binder
-rm -rf Dockerfile
-echo "FROM $IMAGE_TAG:$COMMIT_SHA" > Dockerfile
+rm -f $GITHUB_WORKSPACE/binder/Dockerfile || true
+echo "FROM $IMAGE_TAG:$COMMIT_SHA" > $GITHUB_WORKSPACE/binder/Dockerfile
