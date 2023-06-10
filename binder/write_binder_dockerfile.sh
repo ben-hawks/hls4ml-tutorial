525db6658355a24c2fbc2cf1c25c431409eba61b
“Ben <bhawks@fnal.gov>
2023-06-09 19:46:44 -0500

    fix EoL issues

--- binder/write_binder_dockerfile.sh
+++ binder/write_binder_dockerfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #To make this repo work and uses the existing Docker image within a BinderHub instance, we need to create a shim Dockerfile that Binder/repo2docker picks up on. 
 #This is intended to be ran via a github action
-rm -f $GITHUB_WORKSPACE/binder/Dockerfile || true
+rm -f $GITHUB_WORKSPACE/binder/Dockerfile* || true
 echo "FROM $IMAGE_TAG:$COMMIT_SHA" > $GITHUB_WORKSPACE/binder/Dockerfile
